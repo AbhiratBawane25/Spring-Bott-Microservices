d710b19e3a98fd132313b658f3a6642164b6a400
Abh2511 <abhiratbawane00@gmail.com>
2026-06-30 00:52:57 +0530

    Adding and Practicing the API Gateway

--- 46_Greet_API/src/main/java/com/example/demo/rest/GreetRestController.java
+++ 46_Greet_API/src/main/java/com/example/demo/rest/GreetRestController.java
@@ -1,6 +1,7 @@
 package com.example.demo.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,12 +14,18 @@ public class GreetRestController
 	@Autowired
 	private WelcomeApiFeignClient welcomeClient;
 	
+	@Autowired
+	private Environment env;
+	
+	
 	@GetMapping("/greet")
 	public String greetMsg()
 	{
 		String welcomeResp = welcomeClient.invokeWelcomeApi();
 		
-		String greetResp = " Good Morning..!!";
+	    String port = env.getProperty("server.port");
+		
+		String greetResp = " Good Morning,(" +port+ "), ";
 		
 		return greetResp + welcomeResp;
 	}
---
+++ 47_Cloud_API_Gateway/src/main/java/com/example/demo/filter/MyFilter.java
@@ -0,0 +1,54 @@
+package com.example.demo.filter;
+
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import com.google.common.net.HttpHeaders;
+
+import jakarta.ws.rs.core.Request;
+import reactor.core.publisher.Mono;
+
+
+@Component
+public class MyFilter implements GlobalFilter 
+{
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		
+		System.out.println("filter ( ) - executed");
+		
+		ServerHttpRequest request = exchange.getRequest();
+		
+//		boolean isValid = true;
+//		
+//		if(!isValid) {
+//			throw new RuntimeException("Invalid Request");
+//		}
+		
+		HttpHeaders headers = request.getHeaders();
+		Set<String> keySet = headers.keySet();
+		
+		
+		// Validate Request
+		if(!keySet.contains("secret")) {
+			throw new RuntimeException("Invalid Request");
+		}
+		
+		List<String> list = headers.get("secret");
+		if(!list.get(0).equals("abc@gmail.com")) {
+			throw new RuntimeException("Invalid Token");
+		}
+		
+		
+		return chain.filter(exchange);
+	}
+	
+
+}
